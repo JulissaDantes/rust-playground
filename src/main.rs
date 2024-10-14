317e59b9c3e9695a295c70510d5b02ee4845ccb9
JulissaDantes <julissadcj@gmail.com>
2024-10-14 09:49:59 -0400

    feat: sort by length and letters

--- src/main.rs
+++ src/main.rs
@@ -30,13 +30,38 @@ fn main() {
    let a = 1;
    let b = a.clone();
    println!("{} {}", a, b);
-   let square:[[char;3]; 4] = [['c','y','d'],['p','t','u'],['q','n','e'],['a','m','o']];
-   let w = words::get_words(Some('e'), Some(&square.concat()), square);
-   println!("{} {:?}", w.len(), &w[0..]);//{:?} for the contents
+   let square:[[char;3]; 4] = [['i','k','r'],['p','e','w'],['t','o','l'],['m','n','c']];
+   let mut w = words::get_words(None, Some(&square.concat()), square);
+   sort_words(&mut w);
+   print_grouped_words(&w);
+  // println!("{} {:?}", w.len(), &w[0..]);//{:?} for the contents
 }
 
 //lowercase separated by _
 fn do_stuff(qty: f64, oz: f64) -> f64 {
     println!("{} {}", qty, oz);
     qty * oz
+}
+
+fn sort_words(w: &mut Vec<String>) {
+        // Sort by length and then alphabetically.
+        w.sort_by(|a, b| {
+            let len_cmp = a.len().cmp(&b.len()); // Compare by length first
+            if len_cmp == std::cmp::Ordering::Equal {
+                a.cmp(b) // If lengths are equal, compare alphabetically
+            } else {
+                len_cmp
+            }
+        });
+}
+
+fn print_grouped_words(words: &Vec<String>) {
+    let mut current_length = 0;
+    for word in words {
+        if word.len() != current_length {
+            current_length = word.len();
+            println!("{} letters:", current_length); // Print length header
+        }
+        println!("  {}", word); // Print word indented under the header
+    }
 }
